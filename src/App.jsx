47e99db2d829c2254f70d98c836859c29aa6fc90
Imam Teguh <imam.teguh33@gmail.com>
2025-11-03 12:03:54 +0700

    feat: tambahkan section hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const App = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+      offset: 100
+    });
+  }, []);
+
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,61 @@
+import { Award, Circle, Heart } from "lucide-react"
+import heroImage from '../assets/hero.jpg'
+
+const Hero = () => {
+  return (
+    <section className="relative mt-6 overflow-hidden bg-gray-50 py-12 px-4 sm:px-6 md:px-8 lg:py-16 lg:px-20
+      flex flex-col lg:flex-row items-center justify-center">
+      <div className="flex-1 lg:ml-28 w-full max-w-xl space-y-6 relative z-20">
+        <h2 className="text-4xl sm:text-5xl text-gray-900 text-center lg:text-left"
+          data-aos="fade-right">
+          Professionals{' '}
+          <span className="font-bold text-black block lg:inline">
+            Service <span className="text-green-500">.</span>
+          </span>
+        </h2>
+        <div className="flex gap-3 mt-4 justify-center lg:justify-start"
+          data-aos="fade-right" data-aos-delay="100">
+          <Circle className="text-pink-500 w-5 h-5" />
+          <Circle className="text-yellow-500 w-5 h-5" />
+          <Circle className="text-green-500 w-5 h-5" />
+        </div>
+        <div className="flex flex-col lg:grid lg:grid-cols-2 gap-6 lg:gap-48 items-center lg:items-start mt-8">
+          <div className="bg-white w-72 rounded-3xl p-6 border border-gray-100 shadow-xl lg:shadow-2xl transition"
+            data-aos="zoom-in" data-aos-delay="200">
+            <div className="ml-4 w-10 h-10 flex items-center justify-center rounded-lg bg-pink-100 mb-4">
+              <span className="text-pink-500 text-xl">
+                <Heart className="text-pink-600 w-6 h-6" /> 
+              </span>
+            </div>
+            <h3 className="ml-4 text-xl font-semibold text-gray-800">Service Subtitle One</h3>
+            <p className="ml-4 text-gray-500 text-sm mt-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <div className="bg-white w-72 rounded-3xl p-6 border border-gray-100 shadow-xl lg:shadow-2xl transition"
+            data-aos="zoom-in" data-aos-delay="300">
+            <div className="ml-4 w-10 h-10 flex items-center justify-center rounded-lg bg-yellow-100 mb-4">
+              <span className="text-yellow-500 text-xl">
+                <Award className="text-yellow-600 w-6 h-6" />
+              </span>
+            </div>
+            <h3 className="ml-4 text-xl font-semibold text-gray-800">Service Subtitle Two</h3>
+            <p className="ml-4 text-gray-500 text-sm mt-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 mt-12 lg:mt-0 lg:ml-12 relative w-full flex justify-center"
+        data-aos="fade-left" data-aos-delay="400">
+        <div className="w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-[600px] lg:h-[500px] 
+          overflow-hidden border-8 border-white shadow-lg relative z-10
+          rounded-[60%_40%_30%_60%/60%_30%_60%_40%]">
+          <img src={heroImage} alt="Hero image" className="object-cover w-full h-full"/>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero
